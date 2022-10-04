924f3488baaac4e47a69acaa51c0b3f8d2956664
towbes <hank.wyatt@gmail.com>
2022-10-04 13:07:47 +0900

    fix shm
    Fix shm with help from atom0s on discord!  https://discord.com/channels/1022736642257211393/1022739660054736966/1026602552990642247

--- multisend/multisend.lua
+++ multisend/multisend.lua
@@ -16,6 +16,20 @@ local C     = ffi.C;
 --]]
 ffi.cdef[[
 
+	DWORD GetProcessId(
+		HANDLE Process
+	);
+
+	HANDLE GetCurrentProcess();
+    
+	typedef const char* LPCSTR;
+
+    typedef struct _LPSECURITY_ATTRIBUTES {
+        DWORD  nLength;
+        LPVOID lpSecurityDescriptor;
+        BOOL   bInheritHandle;
+	} LPSECURITY_ATTRIBUTES, *PSECURITY_ATTRIBUTES, *LPSECURITY_ATTRIBUTES;
+
 	HANDLE CreateFileMappingA(
 	HANDLE				  hFile,
 	HANDLE lpFileMappingAttributes,
@@ -41,6 +55,18 @@ ffi.cdef[[
 		HANDLE hObject
 	);
 
+	BOOL FlushViewOfFile(
+  		LPCVOID lpBaseAddress,
+  		SIZE_T  dwNumberOfBytesToFlush
+	);
+
+	enum {
+        FILE_MAP_ALL_ACCESS     = 0x0000F001F,
+        FILE_MAP_READ           = 0x000000004,
+        FILE_MAP_WRITE          = 0x000000002,
+        FILE_MAP_COPY           = 0x000000001,
+    };
+
     enum {
         PAGE_EXECUTE_READ      	= 0x20,
         PAGE_EXECUTE_READWRITE 	= 0x40,
@@ -60,56 +86,112 @@ ffi.cdef[[
 		SEC_WRITECOMBINE = 0x40000000,
     };
 
-	// Data struct to be shared between processes
-	typedef struct _TSharedData {
-		UINT32 test;
-	} TSharedData, *TSharedData;
+	//// Data struct to be shared between processes
+	//typedef struct {
+	//	UINT32 test;
+	//} MMF_Global;
+//
+	////enum multisend_type
+	////{
+	////	all, //Param irrelevant
+	////	others, //Param = sender's process ID
+	////	alliance, //Param = sender's server id
+	////	party, //Param = sender's server id
+	////	group, //Param = group id
+	////	single //Param = process id of target
+	////};
+	//
+	typedef struct
+	{
+		uint32_t Process;
+		uint8_t Name[64];
+		uint8_t Active;
+	} MMF_Name_Single;
+	
+	typedef struct
+	{
+		uint32_t				ProcessID;
+		MMF_Name_Single			Names[100];
+	} MMF_Name;
+	//
+	//typedef struct 
+	//{
+	//	uint8_t			active;
+	//	//multisend_type	type;
+	//	uint32_t		param;
+	//	uint32_t		sender_process_id;
+	//	uint8_t			command[248];
+	//} MMF_ICommand_Single;
+	//
+	//typedef struct 
+	//{
+	//	uint32_t				ProcessID;
+	//	uint32_t				Position;
+	//	MMF_ICommand_Single		Command[100];
+	//} MMF_ICommand;
+	//
+	//typedef struct 
+	//{
+	//	uint32_t				target_process_id;
+	//	uint16_t				lastzone;
+	//	uint16_t				zone;
+	//	float					position_x;
+	//	float					position_z;
+	//	uint32_t				idle_count;
+	//} MMF_IFollow;
+	
+	typedef struct 	{
+		MMF_Name				Name;
+		//MMF_ICommand			Command;
+		//MMF_IFollow				Follow;
+	} MMF_Global;
 
 ]];
 
-local hMap;
-local hShm;
+-- Shared Memory Variables
+local shared    = T{ };
+shared.map      = nil;
+shared.view     = nil;
+shared.mem      = nil;
 
-INVALID_HANDLE_VALUE = ffi.cast("HANDLE",ffi.cast("LONG_PTR",-1));
-local FILE_MAP_ALL_ACCESS = 0xf001f
 
 --[[
 * event: load
 * desc : Called when the addon is being loaded.
 --]]
 hook.events.register('load', 'load_cb', function ()
-	--create LPSECURITY_ATTRIBUTES holder
-	local lpAtt = ffi.new('LPSECURITY_ATTRIBUTES');
-	--shm name
-	local luaName = 'luashm';
-	local shmName = ffi.cast('LPCSTR', luaName);
+    shared.map = C.CreateFileMappingA(ffi.cast('HANDLE', -1), nil, C.PAGE_READWRITE, 0, ffi.sizeof('MMF_Global'), 'daoc_sharedmem');
+    if (shared.map == nil) then
+        error('Failed to create required file mapping.');
+    end
 
-    -- Obtain the shm handle..
-	hMap = ffi.cast('HANDLE', C.CreateFileMappingA(
-		INVALID_HANDLE_VALUE, nil, C.PAGE_READWRITE, 0, 1, shmName
-	));
+    shared.view = C.MapViewOfFile(shared.map, C.FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    if (shared.view == nil) then
+        C.CloseHandle(shared.map);
+        shared.map = nil;
+        error('Failed to create map view.');
+    end
 
+    shared.mem = ffi.cast('MMF_Global*', shared.view);
 
-	if (hMap == nil) then
-		msg = ("map failed");
-		daoc.chat.msg(daoc.chat.message_mode.help, msg)
-		return;
+	--Check if we already have an active process
+	local procId = C.GetProcessId(C.GetCurrentProcess());
+    --Check if this process already has our procId
+	for i=0, 100 do
+		if (shared.mem.Name.Names[i].Active == 1) then
+			if (shared.mem.Name.Names[i].Process == procId) then
+				return;
+			end
+		end
 	end
-
-	
-	hShm = ffi.cast('HANDLE', C.MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 0));
-
-	if (hMap == nil) then
-		msg = ("view failed");
-		daoc.chat.msg(daoc.chat.message_mode.help, msg)
-		return;
+	--Otherwise set our procId to first inactive
+	for i=0, 100 do
+		if (shared.mem.Name.Names[i].Active == 0) then
+			shared.mem.Name.Names[i].Active = 1;
+			shared.mem.Name.Names[i].Process = procId;
+			return;
+		end
 	end
-
-	local tempShm = ffi.new("TSharedData");
-
-	ffi.copy(hShm, tempShm, 1);
-
-
 
 end);
 
@@ -119,9 +201,29 @@ end);
 --]]
 hook.events.register('unload', 'unload_cb', function ()
 
-	C.UnmapViewOfFile(hShm);
-	C.CloseHandle(hMap);
+	local procId = C.GetProcessId(C.GetCurrentProcess());
+    --Set our process to inactive
+	for i=0, 100 do
+		if (shared.mem.Name.Names[i].Active == 1) then
+			if (shared.mem.Name.Names[i].Process == procId) then
+				shared.mem.Name.Names[i].Active = 0;
+				shared.mem.Name.Names[i].Process = 0;
+			end
+			return;
+		end
+	end
+	
+	
+	shared.mem = nil;
 
+    if (shared.view ~= nil) then
+        C.UnmapViewOfFile(shared.view);
+		shared.view = nil;
+    end
+    if (shared.map ~= nil) then
+        C.CloseHandle(shared.map);
+        shared.map = nil;
+    end
 end);
 
 --[[
@@ -139,10 +241,23 @@ hook.events.register('command', 'command_cb', function (e)
     if ((args[1]:any('write') and e.imode == daoc.chat.input_mode.slash) or args[1]:any('/write')) then
         e.blocked = true;
 
-        hook.tasks.oncef(1, function ()
-            hShm.test = hShm.test +1;
-        end);
+		if (shared.mem == nil) then
+			return;
+		end
 
+		local procId = C.GetProcessId(C.GetCurrentProcess());
+		daoc.chat.msg(daoc.chat.message_mode.help, ('ProcID: %d'):fmt(procId));
+		--shared.mem.Name.ProcessID = procId;
+		--shared.mem.Name.Names[0].Process = procId;
+
+		for i=0, 100 do
+			if (shared.mem.Name.Names[i].Active == 0) then
+				shared.mem.Name.Names[i].Active = 1;
+				shared.mem.Name.Names[i].Process = procId;
+				
+				return;
+			end
+		end
         return;
     end
 
@@ -150,12 +265,33 @@ hook.events.register('command', 'command_cb', function (e)
     if ((args[1]:any('read') and e.imode == daoc.chat.input_mode.slash) or args[1]:any('/read')) then
         e.blocked = true;
 
-        hook.tasks.oncef(1, function ()
-            msg = ("#: %d"):fmt(hShm.test);
-			daoc.chat.msg(daoc.chat.message_mode.help, msg)
-        end);
+        if (shared.mem == nil) then
+            return;
+        end
 
+		for i=0, 100 do
+			if (shared.mem.Name.Names[i].Active == 1) then
+				daoc.chat.msg(daoc.chat.message_mode.help, ('Char %d: %d'):fmt(i, shared.mem.Name.Names[i].Process));
+			end
+		end
         return;
     end
+
+	-- Command: /maximize, /minimize
+	if ((args[1]:any('clear') and e.imode == daoc.chat.input_mode.slash) or args[1]:any('/clear')) then
+		e.blocked = true;
+
+		if (shared.mem == nil) then
+			return;
+		end
+
+		--Set everything inactive
+		for i=0, 100 do
+			shared.mem.Name.Names[i].Active = 0;
+			shared.mem.Name.Names[i].Process = 0;
+		end
+
+		return;
+	end
 
 end);
