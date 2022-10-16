83db4d749e37cdba6ca0f39cf7ec8e05137c06ed
towbes <hank.wyatt@gmail.com>
2022-10-17 06:41:47 +0900

    remove ashita license from buttonmaster

--- buttonmaster/buttonmaster.lua
+++ buttonmaster/buttonmaster.lua
@@ -1,25 +1,3 @@
---[[
-* daochook - Copyright (c) 2022 atom0s [atom0s@live.com]
-* Contact: https://www.atom0s.com/
-* Contact: https://discord.gg/UmXNvjq
-* Contact: https://github.com/atom0s
-*
-* This file is part of daochook.
-*
-* daochook is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* daochook is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with daochook.  If not, see <https://www.gnu.org/licenses/>.
---]]
-
 addon.name    = 'buttonmaster';
 addon.author  = 'towbes';
 addon.desc    = 'imgui Buttons Page';
